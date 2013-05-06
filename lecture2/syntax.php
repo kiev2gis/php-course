2062e34e9ccc9f1a413002ff26fe2b2b211b1c6a
Andrey Geonya <a.geonya@gmail.com>
2013-05-06 17:04:07 +0300

    lect renamed

---
+++ lecture2/syntax.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Lecture 2. Syntax</title>
+</head>
+<body>
+    <p>Это будет проигнорировано PHP</p>
+    <?php echo '<b>А это будет обработано PHP</b>'; ?>
+    <p>Это тоже будет проигнорировано PHP</p>
+</body>
+</html>
